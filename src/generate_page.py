784a3aa6d43188d61ec4483e1d2097055d4babab
eraldvelcani <eraldvelcani2003@gmail.com>
2025-06-25 13:18:43 +0200

    final maybe

--- src/generate_page.py
+++ src/generate_page.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return line.strip()
     raise Exception
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path, 'r')
     markdown_content = from_file.read()
@@ -26,6 +26,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html)
+    template_content = template_content.replace('href="/', 'href="{basepath}')
+    template_content = template_content.replace('href="/', 'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != '':
@@ -34,15 +36,15 @@ def generate_page(from_path, template_path, dest_path):
     to_file.write(template_content)
     to_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 def copy_files_recursive(source_dir_path, dest_dir_path):
     if not os.path.exists(dest_dir_path):
--- src/main.py
+++ src/main.py
@@ -3,23 +3,29 @@ from static_to_public import static_to_public
 from generate_page import *
 import os
 import shutil
+import sys
 
 
 def main():
     '''node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)'''
-    
+   
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     static_to_public("/home/erald/static-site/static", "/home/erald/static-site/public")
     
-    if os.path.exists("./public"):
+    if os.path.exists("./docs"):
         print('Deleting public dir...')
-        shutil.rmtree("./public")
+        shutil.rmtree("./docs")
     
     print("Copying static files to public dir...")
-    copy_files_recursive("./static", "./public")
+    copy_files_recursive("./static", "./docs")
 
     print("Generating content...")
-    generate_pages_recursive("./content", "./template.html", "./public")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
+    
 main()
 
